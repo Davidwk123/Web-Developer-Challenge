0ef9899c8139b4d4ddde7ae83730eb18eec61b36
David <welchkeliihd@seattleu.edu>
2022-11-25 22:34:51 -0800

    Added react app

---
+++ react-app/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState }  from 'react';
+import "./App.css"
+
+function App() {
+
+  // Assigning data
+  const [data, setData] = useState("Loading")
+
+  useEffect(() => {
+    const url = "https://www.fishwatch.gov/api/species"
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setData(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Testing to see if collection of json data works
+  // const fishData = data["Species Illustration Photo"]
+  // const fishPhoto = []
+
+  // for (let i in fishData)
+  //   fishPhoto.push(fishData[i]);
+  // console.log(fishPhoto);
+  /* <img src={fishPhoto[0]} alt={fishPhoto[1]}></img> */
+  return (
+    <div className='App'>
+      <header>
+      Hello World
+      </header>
+    </div>
+ 
+  );
+};
+
+export default App;
